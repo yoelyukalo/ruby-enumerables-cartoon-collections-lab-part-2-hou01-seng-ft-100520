6fa02089cbe9f07e03008d85da31a9fe85349214
Yoel Yukalo <yoelyukalo@gmail.com>
2020-09-23 20:02:10 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,7 +14,7 @@ def summon_captain_planet(planeteer_calls)
   while count < planeteer_calls.length do
     word = ""
     planeteer_calls[count] = word
-    new_arr << word.capitalize + !
+    new_arr << planeteer_calls.capitalize + !
     count += 1
   end
   new_arr
