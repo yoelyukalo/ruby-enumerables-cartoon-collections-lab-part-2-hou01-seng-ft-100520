c6bdb2db84b3de007caff4c7e853bb1c30be0237
Yoel Yukalo <yoelyukalo@gmail.com>
2020-09-23 20:16:50 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,7 +19,9 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls(planeteer_calls)
-  
+  count = 0
+  while count < planeteer_calls.length do
+    if planeteer_calls[count].length < 
 end
 
 def find_valid_calls(planeteer_calls)
